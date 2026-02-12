dbb3d5fbb31f69cd359028dc1ae6ce5194443ea6
Howdoicounttoten <squishie4445@gmail.com>
2026-02-12 01:17:50 +0000

    clean {linebreak}

--- binarynumbergenerator.py
+++ binarynumbergenerator.py
@@ -4,6 +4,7 @@ from random import randint
 ## REMOVE PRINT ON LINE 22 IF YOU TRULY WANT A GUESSING GAME 
 
 def generatebinarynumber():
+        linebreak = "=" * 50
         total_correct = 0
         total_attempts = 0
         conswrong = 0
@@ -12,14 +13,14 @@ def generatebinarynumber():
             start = pyfiglet.figlet_format(
                   "        THE BNG",
                   font = "standard")
-            print("============================================================\n"f"{start}")
+            print(f"{linebreak}\n{start}")
             print("                [Binary Number Generator]")
             while True:
                 # Generate ONE number
                 target_int = randint(1, 100)
                 target_bin = f"{target_int:b}"
-                print(f"============================================================"f"\nwhat is : {target_bin} as an interger?\n")
-                print(f"Int: {target_int} (for testing lol idk binary) \n============================================================")
+                print(f"{linebreak}"f"\nwhat is : {target_bin} as an interger?\n")
+                print(f"Int: {target_int} (for testing lol idk binary) \n{linebreak}")
 
                 attempts = 0
 
@@ -36,7 +37,7 @@ def generatebinarynumber():
                     try:
                             guess = int(user_input)
                     except ValueError:
-                        print(f"============================================================\nthat aint a number dumbass. try again what is : {target_bin} ? ")
+                        print(f"{linebreak}\nthat aint a number dumbass. try again what is : {target_bin} ? ")
                         continue
                     # Check if correct
                     if guess == target_int and conswrong < 5 :
@@ -44,7 +45,7 @@ def generatebinarynumber():
                         total_correct += 1
                         attempts += 1
                         conswrong = 0
-                        print(f"============================================================\nWell done Smartass! {target_bin} = {target_int} (took {attempts} attempts)")          
+                        print(f"{linebreak}\nWell done Smartass! {target_bin} = {target_int} (took {attempts} attempts)")          
                         break
                     else: # Wrong answer - keep guessing
                         conswrong += 1
@@ -52,12 +53,12 @@ def generatebinarynumber():
                         total_attempts += 1
                         guesses = (maxwrong - conswrong)
                     if  guess != target_int and conswrong < 5:
-                        print("============================================================\nYOU IDIOT, try again.\n")
+                        print("{linebreak}\nYOU IDIOT, try again.\n")
                         print(f"What is {target_bin} as a number ?") 
-                        print(f"\nYOU HAVE {guesses} GUESSES LEFT!\n============================================================ ")
+                        print(f"\nYOU HAVE {guesses} GUESSES LEFT!\n{linebreak} ")
                     else:
                         if conswrong == 5:
-                            print(f"============================================================\n          FINAL GUESS.\n \nWHAT IS {target_bin} AS A NUMBER ? \n============================================================")
+                            print(f"{linebreak}\n          FINAL GUESS.\n \nWHAT IS {target_bin} AS A NUMBER ? \n{linebreak}")
 
                     if conswrong >= 5 and guess == target_int :
                         print("\nwell done ! got it on the last try!")                 
@@ -70,9 +71,9 @@ def generatebinarynumber():
 
                         gameover = pyfiglet.figlet_format("                        Game Over!",
                         font= "standard")  ##ascii art for game over
-                        print(f"========================================================================================================\n                                      THE ANSWER WAS - {target_int} - DUMBASS")
+                        print(f"{linebreak}{linebreak}\n                                      THE ANSWER WAS - {target_int} - DUMBASS")
                         print(gameover)       
-                        print(f"                              You got {total_correct} correct out of {total_attempts} total attempts. \n========================================================================================================") 
+                        print(f"                              You got {total_correct} correct out of {total_attempts} total attempts. \n{linebreak}{linebreak}") 
                         return False
             
 generatebinarynumber()
